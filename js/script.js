092aa3c1eaf04c87cfbae7111c6354a89e04f217
Noah Huppert <noahhuppert@gmail.com>
2016-11-13 19:48:18 -0500

    +thirsty state,+basic button behavior

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
 /*
-Brainstorming
-=============
-- Hungry
-- Full
-- Thirsty
-- Drunk
-- Sick
-
-- Happy
-- Sad
-
-- Energetic
-- Tired
-
-- Intrigued
-- Inspired
-
 Moods
 =====
-- Hungry
-- Thirsty
-- Happy
-- Sad
-- Tired
+- [X] Hungry
+- [X] Thirsty
+- [ ] Happy
+- [ ] Sad
+- [ ] Tired
  */
+"use strict";
+
+var moods = [
+    "hungry",
+    "thirsty"
+];
+
+var imgEl = document.getElementById("mood-img");
+var mood = 0;
+
+function onTick() {
+    if (mood < moods.length) {
+        mood++;
+    } else {
+        mood = 0;
+    }
+
+    imgEl.src = "img/states/" + moods[mood] + "/" + moods[mood] + ".png";
+}
+
+document.getElementById("chng-mood-btn").onclick = onTick;
