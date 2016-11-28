f48346ec5869c32f5ea3da1259e2fc608234fe8c
Noah Huppert <noahhuppert@gmail.com>
2016-11-28 08:58:47 -0500

    coded state class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// HTML elements for future use
+var stateImgEl = document.getElementById("state-img");
+var currentStateEl = document.getElementById("current-state");
+
 /**
  * An object which holds the configuration for one state
  *
@@ -25,7 +29,6 @@
 var states = [
     {
         id: "hungry",
-        weight: 1.0,
         probabilities: {
             self: 0.8,
             thirsty: 0.35,
@@ -36,7 +39,6 @@ var states = [
     },
     {
         id: "thirsty",
-        weight: 1.0,
         probabilities: {
             self: 0.7,
             hungry: 0.4,
@@ -47,7 +49,6 @@ var states = [
     },
     {
         id: "happy",
-        weight: 1.0,
         probabilities: {
             self: 0.5,
             hungry: 0.3,
@@ -58,7 +59,6 @@ var states = [
     },
     {
         id: "sad",
-        weight: 1.0,
         probabilities: {
             self: 0.3,
             hungry: 0.25,
@@ -69,7 +69,6 @@ var states = [
     },
     {
         id: "tired",
-        weight: 1.0,
         probabilities: {
             self: 0.5,
             hungry: 0.4,
@@ -105,12 +104,12 @@ var stimuli = [
     },
     {
         id: "election-results",
-        state: "happy",
+        state: "sad",
         effect: -0.7,
     },
     {
         id: "balloons",
-        state: "sad",
+        state: "happy",
         effect: -0.3
     },
     {
@@ -120,35 +119,287 @@ var stimuli = [
     }
 ];
 
-// Custom HTML components declaration using VueJS
-// Documentation on VueJS Components: https://vuejs.org/v2/guide/components.html
 /**
- * Pest State custom component. Displays media to represent
- * the pest state provided.
+ * Class which represents Virtual Pest's state
+ * @param (StateConf[]} statesConf Array of states configuration
+ * @param {StimulusConf[]} stimuliConf Array of stimuli configuration
+ * @constructor
  */
-Vue.component("x-state", {
-    // Declare properties that custom element can accept
-    props: {
-        // {string} state - Id of state to display
-        state: {
-            type: String,
-            required: true
-        }
-    },
-    // HTML to display for component
-    template: '<img class="x-state-img" v-bind:src="mediaUrl" />',
-    // Data properties
-    computed: {
-        /**
-         * Function which computes the value for the `mediaUrl` variable.
-         * @returns {string} The path to the state image.
-         */
-        mediaUrl: function() {
-            return "img/states/" + this.state + "/" + this.state + ".png";
-        }
-    }
-});
+function State(statesConf, stimuliConf) {
+    var self = this;
 
-var app = new Vue({
-    el: "#app"
-});
+    if (statesConf === undefined || stimuliConf === undefined) {
+        throw new Error("Two arguments expected in State constructor");
+    }
+
+    // Interpret config
+    self.states = [];
+    for (var i = 0; i < statesConf.length; i++) {
+        var conf = statesConf[i];
+
+        // State object to be added
+        var state = {
+            // State Id
+            id: conf.id,
+
+            // All values for probabilities
+            probabilities: {
+
+                // Values used to calculate the actual probability values
+                parameters: {
+
+                    // Probability that state will stay the same (Change to its *self*)
+                     self: {
+                         // Probability is calculated with value * modifier
+                         value: conf.probabilities.self,
+                         modifier: 1.0
+                    },
+
+                    // Holds probability values that state will *switch*
+                    switch: {}
+                },
+
+                // Calculated probability values go here
+                ranges: {},
+
+                // Keep track of the `probabilities.parameters.self.modifier` value so we
+                // don't have to update if there isn't a change
+                _lastRangesCalcModVal: -1
+            }
+        };
+
+
+        for (var key in conf.probabilities) {
+            if (key === "self") continue;
+            state.probabilities.parameters.switch[key] = conf.probabilities[key];
+        }
+
+        self.states.push(state);
+    }
+
+    self.stimuli = stimuliConf;
+
+    // State
+    // These two variables should be kept consistent
+    self.currentStateId = "happy";
+    self.currentStateI = 2;
+
+    /**
+     * Step simulation of state once. This will look at the current
+     * state and determine the next state.
+     */
+    self.step = function() {
+        // Calculate probability ranges
+        self.calcRanges();
+
+        // Find next state
+        var state = self.states[self.currentStateI];
+
+        var random = Math.random();
+
+        var lastValue = 0;// Used as lower bound
+        var nextStateId = "";
+        for (var key in state.probabilities.ranges) {
+            var value = state.probabilities.ranges[key];// Used as upper bound
+
+            // If value is within range
+            if (random > lastValue && random <= value) {
+                // Save key and exit loop
+                nextStateId = key;
+                break;
+            }
+
+            lastValue = value;
+        }
+
+        // Set next state
+        if (nextStateId !== "self") {// If next state is "self" than don't change a thing
+            self.currentStateId = nextStateId;
+            var nextStateI = -1;
+
+            var i = 0;
+            for (var key in self.states) {
+                if (key === nextStateId) {
+                    self.currentStateI = i;
+                }
+
+                i++;
+            }
+
+            self._mapDOM();
+        }
+    };
+
+    /**
+     * Calculates a set of ranges that are used to determine the
+     * probability of the state changing to another state or staying
+     * the same.
+
+     * Note: The term "self" is used to describe the next state
+     * being the same as the current state (Because the next state turns into
+     * its "self").
+     *
+     * This function looks at the current state object, more
+     * particularly the values in `probabilities.parameters`. It uses those
+     * values to compute the values for `probabilities.ranges`.
+     *
+     * Explanation of values:
+     * - `probabilities` - Values related to state changes
+     *     - `parameters` - Values that tune the generation process
+     *     for the values in `ranges`
+     *         - `self` - Values related to the next state being
+     *         the same as the current
+     *             - `modifier` - Value that program tweaks to make `self`
+     *             range change on user input
+     *             - `value` - Probability (0.0 - 1.0) that next state will
+     *             be "self"
+     *         - `switch` - Values related to the next state being
+     *         different from the current state
+     *             - This is a object which contains a key for every other mood
+     *             (ex., "happy" or "sad" if state is "hungry")
+     *             - The values of these keys is the same as the `self.value`
+     *             value. It is the probability (0.0 - 1.0) that the next state
+     *             will be the state identified in the key
+     *     - `ranges` - An object which holds a series of values, 0.0 from
+     *     1.0,
+     */
+    self.calcRanges = function() {
+        // Convenience vars
+        var state = self.states[self.currentStateI];
+        var parameters = state.probabilities.parameters;
+
+        // Make sure we haven't already calculated the values
+        if (state.probabilities._lastRangesCalcModVal !== parameters.self.modifier) {
+            // Calculate self value
+            var selfValue = parameters.self.value * parameters.self.modifier;
+
+            // Calculate switch values
+            var switchSpace = 1 - selfValue;// Space available for range after big switch value
+            var switchValues = {};
+
+            for (var key in parameters.switch) {
+                switchValues[key] = parameters.switch[key] * switchSpace;
+            }
+
+            // Assign values
+            var runningRange = 0;
+            for (var key in switchValues) {
+                runningRange += switchValues[key];
+                state.probabilities.ranges[key] = runningRange;
+            }
+
+            state.probabilities.ranges["self"] = 1.0;
+
+            // Mark that we have calculated the values for this case
+            state.probabilities._lastRangesCalcModVal = parameters.self.modifier;
+
+            // Store most recent version of state
+            console.log(state);
+            self.states[self.currentStateI] = state;
+        }
+    };
+
+    /**
+     * Maps values for current state onto DOM
+     */
+    self._mapDOM = function() {
+        // State image
+        stateImgEl.src = "img/states/" + self.currentStateId + "/"  + self.currentStateId + ".png";
+
+        // Current state label
+        currentStateEl.innerText = prettifyString(self.currentStateId);
+    };
+
+    /**
+     * Apply the effects of the given stimulus to the state
+     * @param id Stimulus id
+     */
+    self.applyStimulus = function(id) {
+        // Find stimulus config that we clicked on
+        var stimulus;
+        for (var i = 0; i < self.stimuli.length; i++) {
+            var s = self.stimuli[i];
+            if (s.id === id) {
+                stimulus = s;
+                break;
+            }
+        }
+
+        if (stimulus === undefined) {
+            throw new Error("Can not find stimulus with id: \"" + id + '\"');
+        }
+
+        // Find state stimulus refers to
+        var state;
+        for (var i = 0; i < self.states.length; i++) {
+            var s = self.states[i];
+            if (s.id === stimulus.state) {
+                state = s;
+                break;
+            }
+        }
+
+        // Apply state effect
+        // The State self probability is multiplied by the modifier
+        // Adding the
+        state.probabilities.parameters.self.modifier += stimulus.effect;
+        console.log(state.probabilities.parameters.self.modifier);
+    };
+
+    // Map first state
+    self._mapDOM();
+}
+
+function prettifyString(text) {
+    text = text.replace("-", " ");
+
+    var parts = text.split(" ");
+    for (var i = 0; i < parts.length; i++) {
+        var part = parts[i];
+        parts[i] = part[0].toUpperCase() + part.slice(1);
+    }
+
+    return parts.join(" ");
+}
+
+// Create state instance
+var state = new State(states, stimuli);
+
+setInterval(function() {
+    state.step();
+}, 1000);
+
+// Generate HTML for stimuli menu
+var html = "";
+for (var i = 0; i < stimuli.length; i++) {
+    var stimulus = stimuli[i];
+
+    html += "<div class=\"stimulus\" " +
+                 "data-stimulus-id=\"" + stimulus.id +
+              "\" onclick=\"onStimulusElClick(this)\">";
+
+    var stimulusImgUrl = "img/stimuli/" + stimulus.id + "/" + stimulus.id + ".png";
+    html += "<img src=\"" + stimulusImgUrl + "\" />";
+
+    html += "<div class=\"stimulus-name\">";
+    html += prettifyString(stimulus.id);
+    html += "</div>";
+
+    html += "<div class=\"stimulus-effect\">";
+
+    html += "<div class=\"sign\">" + (stimulus.effect < 0 ? "+" : "") + "</div>";
+    html += (stimulus.effect * -1) + " "  + prettifyString(stimulus.state);
+
+    html += "</div>";
+
+    html += "</div>";
+}
+
+document.getElementById("stimuli-content").innerHTML = html;
+
+// Click listener for stimuli
+function onStimulusElClick(self) {
+    var id = self.getAttribute("data-stimulus-id");
+
+    state.applyStimulus(id);
+}
